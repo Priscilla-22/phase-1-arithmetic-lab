6d3ffb6e50492939b7273639eafaa9d50336bf1b
Priscilla-22 <priscillamw123@gmail.com>
2023-12-11 22:53:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+let num1 = 1
+let num2 = 62
+let multiply = num1 * num2;
+
+console.log(multiply);
+// result 62;
+
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+//return num > 0
+
+let num3 = 22
+let num4 = 9
+let mod = num3 % num4;
+console.log(mod);
+//result 4
+
+let max = Math.max(10, 16, 12, 20, 17);
+console.log(max);
+// return 20
